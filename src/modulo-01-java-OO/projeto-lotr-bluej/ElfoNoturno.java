e6c68d5d4a69e7348c9d191860acc8ac15aa7654
Murillo Peteffi <Murillo Peteffi>
2016-04-11 20:52:27 -0300

    doc(ElfoNoturno): Resposta da questão 7.2.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturno.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturno.java
@@ -15,10 +15,20 @@ public class ElfoNoturno extends Elfo {
             experiencia=experiencia+3;
             gimli.perderVida();
             this.vida=vida-((5*this.vida)/100); 
-            /*mais correto seria a variável vida estar em double, na definição,
-             e praticamente todos os metodos e classes deveriam ser alteradas.
-             Porém, considerando que o método é inconsistente de qualquer maneira,
-             mantenho assim para alterar no próximo commit. */
+            /*Mais correto seria a variável vida estar em double, na definição,
+              e praticamente todos os metodos e classes deveriam ser alteradas.
+              Porém, considerando que o método é inconsistente de qualquer maneira,
+              mantenho assim para alterar no próximo commit. */
+             
+             
+            /*A partir da pergunta da questão 7.2, cabe ressaltar que calcular
+              decaimento de valores a partir de subtrações recorrentes por uma
+              mesma fração do número, resulta numa série numérica que, embora 
+              converga para zero, só alcança o valor na quantidade de infinitas
+              subtrações. Desta forma, considerando a impossibilidade de infinitas
+              operações e desconsiderando o arredondamento de valores que possivelmente
+              possam ser feitas pelo sistema, o Elfo Noturno nunca morre por atirar
+              flechas.*/
             } 
     
 }
