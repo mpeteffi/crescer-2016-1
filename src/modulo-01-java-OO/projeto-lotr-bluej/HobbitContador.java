bd756ad0004df81dc8fd697c497705e4791d24ad
Murillo Peteffi <Murillo Peteffi>
2016-04-12 21:37:23 -0300

    refactor(HobbitContador): calculo de mmc extraído para método, facilitando leitura de código.

--- src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
@@ -7,21 +7,9 @@ public class HobbitContador
     public int calcularDiferenca(ArrayList<ArrayList<Integer>> array){
         int diferenca=0;
         for(int i=0;i<array.size();i++){
-            int produto=0, mmc=1, fator=2,
-            n1 = array.get(i).get(0),
-            n2 = array.get(i).get(1);
-            //calcular produto
-            produto = n1*n2;
-            //calcular mmc
-            while(n1!=1||n2!=1){
-                if(n1%fator==0||n2%fator==0){
-                    if(n1%fator==0){n1=n1/fator;}
-                    if(n2%fator==0){n2=n2/fator;}
-                    mmc=mmc*fator;
-                }else{fator++;}
-            }
-            //calcular diferença entre produto e mmc
-            diferenca += Math.abs(produto - mmc);
+            int num1 = array.get(i).get(0), num2 = array.get(i).get(1),
+            produto = num1*num2;
+            diferenca += Math.abs(produto - calcularMmc(num1,num2));
         }
         return diferenca;
     }
@@ -50,5 +38,17 @@ public class HobbitContador
         }
 
         return multiplos;
+    }
+    
+    public static int calcularMmc(int n1, int n2){
+        int mmc=1, fator=2;
+        while(n1!=1||n2!=1){
+                if(n1%fator==0||n2%fator==0){
+                    if(n1%fator==0){n1=n1/fator;}
+                    if(n2%fator==0){n2=n2/fator;}
+                    mmc=mmc*fator;
+                }else{fator++;}
+        }
+        return mmc;
     }
 }
