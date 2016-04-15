530571fb47723804d059f458cfa9a98dc312b90c
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-04-15 14:49:56 -0300

    test(AtaqueIntercalado): criar metodos para facilitar testes, melhorar leitura e entendimento do código.

--- src/modulo-01-java-OO/projeto-lotr-bluej/AtaqueIntercaladoTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/AtaqueIntercaladoTest.java
@@ -9,29 +9,16 @@ public class AtaqueIntercaladoTest
 {
     @Test
     public void atacarHordaAtaqueIintercaladoTodosVivos(){
-        //criar exercito com 6 elfos
-        ExercitoDeElfos e1 = new ExercitoDeElfos();
-        Elfo ev1 = new ElfoVerde("ev1");
-        Elfo ev2 = new ElfoVerde("ev2");
-        Elfo ev3 = new ElfoVerde("ev3");
-        Elfo en1 = new ElfoNoturno("en1");
-        Elfo en2 = new ElfoNoturno("en2");
-        Elfo en3 = new ElfoNoturno("en3");
-        e1.alistarUmElfo(en1);
-        e1.alistarUmElfo(en2);
-        e1.alistarUmElfo(ev1);
-        e1.alistarUmElfo(ev2);
-        e1.alistarUmElfo(en3);
-        e1.alistarUmElfo(ev3);
-        //criar horda de dwarves com 2 elfos
-        ArrayList<Dwarf> hordaDeAnoes = new ArrayList<>();
-        Dwarf a1 = new Dwarf("a1");
-        Dwarf a2 = new Dwarf("a2");
-        hordaDeAnoes.add(a1);
-        hordaDeAnoes.add(a2);
+        //criar exercito com 3 ElfosVerdes e 3 ElfosNoturnos
+        ExercitoDeElfos e1 = criarExercitoDeElfos(3, 3);
+        
+        //criar horda de dwarves com 2 anoes
+        ArrayList<Dwarf> hordaDeAnoes = criarHordaDwarves(2);
+        
         //atacar usando arteDaGuerra
         EstrategiaDeAtaque estrategia = new AtaqueIntercalado(e1.getExercito());
         ArrayList<Elfo> ordemAtaque = estrategia.atacarHorda(hordaDeAnoes);
+        
         //comparar ordem de ataque com o esperado
         assertTrue(ordemAtaque.get(0)instanceof ElfoVerde && 
             ordemAtaque.get(2)instanceof ElfoNoturno || 
@@ -41,6 +28,33 @@ public class AtaqueIntercaladoTest
             ordemAtaque.get(6)instanceof ElfoNoturno || 
             ordemAtaque.get(4)instanceof ElfoNoturno &&
             ordemAtaque.get(6)instanceof ElfoVerde);
+        assertTrue(ordemAtaque.get(6)instanceof ElfoVerde && 
+            ordemAtaque.get(8)instanceof ElfoNoturno || 
+            ordemAtaque.get(6)instanceof ElfoNoturno &&
+            ordemAtaque.get(8)instanceof ElfoVerde);
 
+    }
+    
+    public ExercitoDeElfos criarExercitoDeElfos(int verdes, int noturnos){
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        
+        for(int i=1;i<=verdes;i++){
+            exercito.alistarUmElfo(new ElfoVerde("ElfoVerde" + i));
+        }
+        for(int i=1;i<=noturnos;i++){
+            exercito.alistarUmElfo(new ElfoNoturno("ElfoNoturno" + i));
+        }
+    
+        return exercito;
+    }
+    
+    public ArrayList<Dwarf> criarHordaDwarves(int anoes){
+        ArrayList<Dwarf> horda = new ArrayList<>();
+        
+        for(int i=1;i<=anoes;i++){
+            horda.add(new Dwarf("Anao" + i));
+        }
+    
+        return horda;
     }
 }
