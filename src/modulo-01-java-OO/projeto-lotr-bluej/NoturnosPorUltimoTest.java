76f48043367c685ebd9a2bde7c91e0bdcd7dc3a7
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-04-15 15:47:24 -0300

    test(NoturnosPorUltimo): adicionar cenários de teste.

--- src/modulo-01-java-OO/projeto-lotr-bluej/NoturnosPorUltimoTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/NoturnosPorUltimoTest.java
@@ -27,14 +27,48 @@ public class NoturnosPorUltimoTest{
         }
     }
     
+    @Test
+    public void atacarHordaDeElfosNoturnosUltimo4e1(){
+        ExercitoDeElfos e1 = criarExercitoDeElfos(4, 1);
+        
+        ArrayList<Dwarf> hordaDeAnoes = criarHordaDwarves(2);
+        
+        EstrategiaDeAtaque estrategia = new NoturnosPorUltimo(e1.getExercito());
+        ArrayList<Elfo> ordemAtaque = estrategia.atacarHorda(hordaDeAnoes);
+        
+        for(int i=0;i<8;i++){
+            assertTrue(ordemAtaque.get(i)instanceof ElfoVerde);
+        }
+        for(int i=8;i<10;i++){
+            assertTrue(ordemAtaque.get(i)instanceof ElfoNoturno);
+        }
+    }
+    
+    @Test
+    public void atacarHordaDeElfosNoturnosUltimo5e3(){
+        ExercitoDeElfos e1 = criarExercitoDeElfos(5, 3);
+        
+        ArrayList<Dwarf> hordaDeAnoes = criarHordaDwarves(1);
+        
+        EstrategiaDeAtaque estrategia = new NoturnosPorUltimo(e1.getExercito());
+        ArrayList<Elfo> ordemAtaque = estrategia.atacarHorda(hordaDeAnoes);
+        
+        for(int i=0;i<5;i++){
+            assertTrue(ordemAtaque.get(i)instanceof ElfoVerde);
+        }
+        for(int i=5;i<8;i++){
+            assertTrue(ordemAtaque.get(i)instanceof ElfoNoturno);
+        }
+    }
+    
     private ExercitoDeElfos criarExercitoDeElfos(int verdes, int noturnos){
         ExercitoDeElfos exercito = new ExercitoDeElfos();
         
-        for(int i=1;i<=verdes;i++){
-            exercito.alistarUmElfo(new ElfoVerde("ElfoVerde" + i));
-        }
         for(int i=1;i<=noturnos;i++){
             exercito.alistarUmElfo(new ElfoNoturno("ElfoNoturno" + i));
+        }
+        for(int i=1;i<=verdes;i++){
+            exercito.alistarUmElfo(new ElfoVerde("ElfoVerde" + i));
         }
     
         return exercito;
