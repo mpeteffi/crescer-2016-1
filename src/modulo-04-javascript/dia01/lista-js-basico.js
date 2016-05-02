746cc3675aaa5bcaf1fee1b02c260e2c575cc71d
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-05-02 17:41:39 -0300

    feat(lista-js-basico): adicionar resolução dos exercicios 1 e 2 do dia 01 do módulo javascript.

---
+++ src/modulo-04-javascript/dia01/lista-js-basico.js
@@ -0,0 +1,18 @@
+function daisyGame(numeroPetalas){
+    ehNumero = typeof numeroPetalas === 'number';
+    ehInteiro = numeroPetalas%1 === 0;
+    if(ehNumero && ehInteiro)
+    return numeroPetalas%2===0 ? 'Love Me Not' : 'Love Me';
+    else return ("Por favor, insira um número natural.")
+}
+
+function maiorTexto(lista) {
+    var tamanho = 0;
+    for (var i=0; i<lista.length; i++){
+        var palavra = lista[i];
+        if(palavra.length > tamanho){
+        tamanho = palavra.length;
+        var index = i;}
+    }
+    return (lista[index] + " com " + tamanho + " letras.")
+}
