fa915593d253e0ad14458e539c41a23c3dac8d60
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-05-03 16:38:47 -0300

    feat(obterDoadores): adicionar resolução exercicio 1 com teste.

--- src/modulo-04-javascript/dia02/the-gold-saints-specs.js
+++ src/modulo-04-javascript/dia02/the-gold-saints-specs.js
@@ -1,7 +1,9 @@
 'use strict';
 
 describe('Ex 1. Doadores de sangue', function() {
-  it('obterDoadores retorna array vazio', function() {
-    expect(obterDoadores()).toEqual([]);
+  it('obterDoadores retorna todos com sangue O (4,8,11)', function() {
+    expect(obterDoadores()).toEqual([goldSaints[4],goldSaints[8],goldSaints[11]]);
+  });
+});
   });
 });
