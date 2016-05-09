de594fb5b3039843fe0d77a6e22418503dd37ad7
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-05-09 11:46:54 -0300

    feat(Megaman): adicionar upgrade botas de super velocidade

---
+++ src/modulo-05-dotnet/Megaman/Megaman/BotasDeSuperVelocidade.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioMegaman
+{
+    public class BotasDeSuperVelocidade : IUpgrade
+    {
+        public int BonusAtaque
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+        public int BonusDefesa
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+        public string TipoUpgrade
+        {
+            get
+            {
+                return "Lendário";
+            }
+        }
+    }
+}
