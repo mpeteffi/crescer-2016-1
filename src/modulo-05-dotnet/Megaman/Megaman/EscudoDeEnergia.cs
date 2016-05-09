4f5aec375fcab508c22c57c94c2dbc2c3fd5f0e3
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-05-09 11:47:28 -0300

    feat(Megaman): adicionar upgrade escudo de energia

---
+++ src/modulo-05-dotnet/Megaman/Megaman/EscudoDeEnergia.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioMegaman
+{
+    public class EscudoDeEnergia : IUpgrade
+    {
+        public int BonusAtaque
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public int BonusDefesa
+        {
+            get
+            {
+                return 2;
+            }
+        }
+
+        public string TipoUpgrade
+        {
+            get
+            {
+                return "UpgradeDeDefesa";
+            }
+        }
+    }
+}
