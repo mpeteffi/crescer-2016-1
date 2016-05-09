274ff688fb98e523a9b9fcf863728fcffa9075cb
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-05-09 11:47:42 -0300

    feat(Megaman): adicionar Rush.

---
+++ src/modulo-05-dotnet/Megaman/Megaman/Rush.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioMegaman
+{
+    public class Rush : Robo, IUpgrade
+    {
+        public int BonusAtaque
+        {
+            get
+            {
+                return Ataque;
+            }
+        }
+
+        protected override int MaxUpgrades
+        {
+            get { return 2; }
+        }
+
+        public int BonusDefesa
+        {
+            get
+            {
+                return 3;
+            }
+        }
+
+        public override string Nome
+        {
+            get
+            {
+                return "Rush";
+            }
+        }
+
+        public string TipoUpgrade
+        {
+            get
+            {
+                return "Lendário";
+            }
+        }
+
+        protected override int Ataque
+        {
+            get
+            {
+                return 4 + BonusEquipAtaque;
+            }
+        }
+
+        protected override int Defesa
+        {
+            get
+            {
+                return 3 + BonusEquipDefesa;
+            }
+        }
+    }
+}
