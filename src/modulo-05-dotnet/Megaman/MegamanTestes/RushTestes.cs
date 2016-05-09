1b1c30b0eafa0569a8567e237cfb6bd3f98a0adc
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-05-09 12:06:00 -0300

    test(Rush): adicionar testes para rush com upgrades.

--- src/modulo-05-dotnet/Megaman/MegamanTestes/RushTestes.cs
+++ src/modulo-05-dotnet/Megaman/MegamanTestes/RushTestes.cs
@@ -38,5 +38,48 @@ namespace MegamanTestes
             Assert.AreEqual(rush.toString(), "Nome: {Rush}, Vida: {100}, Ataque: {4}, Defesa: {3}");
         }
 
+        [TestMethod]
+        public void RushRecebeCanhaoPlasmaAtaqueCausa6Dano()
+        {
+            var rush = new Rush();
+            var canhao = new CanhaoDePlasma();
+            var bot = new Bot();
+            rush.EquiparUpgrade(canhao);
+            rush.Atacar(bot);
+            Assert.AreEqual(bot.Vida, 94);
+        }
+
+        [TestMethod]
+        public void RushRecebeEscudoDeEnergiaTomaZeroDano()
+        {
+            var rush = new Rush();
+            var escudo = new EscudoDeEnergia();
+            var bot = new Bot();
+            rush.EquiparUpgrade(escudo);
+            bot.Atacar(rush);
+            Assert.AreEqual(bot.Vida, 100);
+        }
+
+        [TestMethod]
+        public void RushRecebeLendarioToma1Causa5Dano()
+        {
+            var rush = new Rush();
+            var botas = new BotasDeSuperVelocidade();
+            var bot = new Bot();
+            rush.EquiparUpgrade(botas);
+            rush.Atacar(bot);
+            bot.Atacar(rush);
+            Assert.AreEqual(bot.Vida, 95);
+            Assert.AreEqual(rush.Vida, 99);
+        }
+
+        [TestMethod]
+        public void RushEquipaRushTem8Dano6Defesa()
+        {
+            var rush = new Rush();
+            var rushEquip = new Rush();
+            rush.EquiparUpgrade(rushEquip);
+            Assert.AreEqual(rush.toString(), "Nome: {Rush}, Vida: {100}, Ataque: {8}, Defesa: {6}");
+        }
     }
 }
