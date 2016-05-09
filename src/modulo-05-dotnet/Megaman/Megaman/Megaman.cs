ada29c391f6afcaad99e96ddf1dcc0578b8b8672
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-05-09 11:18:08 -0300

    feat(Megaman): adiciona classe Megaman.

--- src/modulo-05-dotnet/Megaman/Megaman/Megaman.cs
+++ src/modulo-05-dotnet/Megaman/Megaman/Megaman.cs
@@ -6,15 +6,29 @@ using System.Threading.Tasks;
 
 namespace ExercicioMegaman
 {
-    public class Megaman : Robo
+    public class MegaMan : Robo
     {
-        public Megaman()
+        public override string Nome
         {
-
+            get
+            {
+                return "Megaman";
+            }
+        }
+        protected override int Ataque
+        {
+            get
+            {
+                return 6 + BonusEquipAtaque;
+            }
         }
 
-
-
-
+        public virtual void Atacar(Robo robo)
+        {
+            if (Vida <= 30)
+                robo.RecebeDano(3 + Ataque);
+            else
+                robo.RecebeDano(Ataque);
+        }
     }
 }
--- src/modulo-05-dotnet/Megaman/Megaman/Robo.cs
+++ src/modulo-05-dotnet/Megaman/Megaman/Robo.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JogoMegaMan
+namespace ExercicioMegaman
 {
     public abstract class Robo
     {
