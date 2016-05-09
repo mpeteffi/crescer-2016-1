69824ac4fc16680216d53f0b69fb602e47ad2b40
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-05-09 11:30:00 -0300

    test(Megaman): adicionar testes para Bot.

---
+++ src/modulo-05-dotnet/Megaman/MegamanTestes/BotTestes.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExercicioMegaman;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MegamanTestes
+{
+        [TestClass]
+        public class BotTestes
+        {
+            [TestMethod]
+            public void BotTomaCincoDeDano()
+            {
+                var bot = new Bot();
+                var botDois = new Bot();
+                botDois.Atacar(bot);
+                Assert.AreEqual(bot.Vida, 95);
+            }
+
+            [TestMethod]
+            public void BotCausaCincoDeDano()
+            {
+                var bot = new Bot();
+                var botDois = new Bot();
+                bot.Atacar(botDois);
+                Assert.AreEqual(botDois.Vida, 95);
+            }
+
+            [TestMethod]
+            public void BotToString()
+            {
+                var bot = new Bot();
+                Assert.AreEqual(bot.toString(), "Nome: {Bot}, Vida: {100}, Ataque: {5}, Defesa: {0}");
+            }
+        }
+}
