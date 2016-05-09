19069feca1bff9a6eb8fcf191a90b0f8e087edd1
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-05-09 11:47:11 -0300

    feat(Megaman): adicionar upgrade canhao de plasma

---
+++ src/modulo-05-dotnet/Megaman/Megaman/CanhaoDePlasma.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioMegaman
+{
+    public class CanhaoDePlasma : IUpgrade
+    {
+        public int BonusAtaque
+        {
+            get
+            {
+                return 2;
+            }
+        }
+
+        public int BonusDefesa
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public string TipoUpgrade
+        {
+            get
+            {
+                return "UpgradeDeAtaque";
+            }
+        }
+    }
+}
