093fd18fee3b1657e68a32590dcefaeb8f05e24c
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-05-10 17:07:00 -0300

    feat(MVC): adicionar exemplos de aula.

---
+++ src/modulo-05-dotnet/Aula 3/Exemplos/01 - MVC/MVC/MVC/Controllers/ExemploController.cs
@@ -0,0 +1,68 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class ExemploController : Controller
+    {
+        public ActionResult Index(int? id)
+        {
+            var personagens = new List<PersonagemModel>()
+                {
+                    new PersonagemModel()
+                    {
+                        Nome = "Sasuke",
+                        Id = 16,
+                        Fodao = false
+                    },
+                    new PersonagemModel()
+                    {
+                        Nome = "Vegeta",
+                        Id = 30,
+                        Fodao = true
+                    },
+                    new PersonagemModel()
+                    {
+                        Nome = "Patrik",
+                        Id = 11,
+                        Fodao = true
+                    },
+                    new PersonagemModel()
+                    {
+                        Nome = "Pepe",
+                        Id = 1,
+                        Fodao = true
+                    }
+                };
+
+            ViewBag.MensagemBoasVindas = "Olá, seja bem vindo!";
+
+            if (id == null)
+            {
+                TempData["Erro"] = "Você não informou o ID, tente novamente mais tarde!";
+
+                return RedirectToAction("Index", "Teste");
+            }
+            else
+            {
+                var personagemEscolhido = personagens.FirstOrDefault(x => x.Id == id);
+
+                return View(personagemEscolhido);
+            }
+
+            //return RedirectToAction("Index", "Teste");
+            //return View(personagens);
+            //return View("HelloWorld");
+            //return View("~/Views/Erros/NotFound.cshtml");
+        }
+
+        public ActionResult UsandoLayout()
+        {
+            return View();
+        }
+    }    
+}
---
+++ src/modulo-05-dotnet/Aula 3/Exemplos/01 - MVC/MVC/MVC/Models/PersonagemModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class PersonagemModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }        
+        public bool Fodao { get; set; }
+    }
+}
