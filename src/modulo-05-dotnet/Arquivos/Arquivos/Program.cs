a1581dcd6fe7108217f519af9522d997c9d72044
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-05-10 12:30:21 -0300

    doc(VS): arquivos secundarios.

---
+++ src/modulo-05-dotnet/Arquivos/Arquivos/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var arquivo = @"C:\Users\murillo.peteffi\Desktop\arquivo.txt";
+            StreamReader stream = null;
+
+            //try
+            //{
+            //    stream = new StreamReader(arquivo);
+            //    string linha;
+            //    do
+            //    {
+            //        linha = stream.ReadLine();
+            //    } while (linha != null);
+            //    stream.Dispose();
+            //}
+            //finally
+            //{
+            //    stream.Dispose();
+            //}
+
+            
+
+            using (var stream = new StreamReader(arquivo))
+            {
+                string linha;
+                linha.Append
+                linha = stream.ReadLine();
+            }
+
+
+
+
+        }
+    }
+}
---
+++ src/modulo-05-dotnet/Exception/Exception/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            try
+            {
+                Console.WriteLine("Digite um número: ");
+                var numero = Console.ReadLine();
+                var numeroConvertido = Convert.ToInt32(numero);
+                if (numeroConvertido == 13) throw new ArgumentException("Você digitou o numero do azar.");
+                Console.WriteLine("O número convertido é: " + numeroConvertido);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Você deve digitar apenas números!");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("O numero deve estar entre -2147483648 e 2147483648.");
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
