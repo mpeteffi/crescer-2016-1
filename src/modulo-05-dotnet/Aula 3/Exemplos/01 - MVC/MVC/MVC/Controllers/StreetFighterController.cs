2927fb1f05522d5c684a4e0622604f4f9a926366
Murillo Peteffi <Murillo Peteffi>
2016-05-10 23:20:41 -0300

    feat(MVC-StreetFighter): adicionar action para view sobre mim.

--- src/modulo-05-dotnet/Aula 3/Exemplos/01 - MVC/MVC/MVC/Controllers/StreetFighterController.cs
+++ src/modulo-05-dotnet/Aula 3/Exemplos/01 - MVC/MVC/MVC/Controllers/StreetFighterController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,7 +16,28 @@ namespace MVC.Controllers
 
         public ActionResult FichaTecnica()
         {
-            return View("FichaTecnica");
+            return View();
+        }
+
+        public ActionResult SobreMim()
+        {
+            var data = new DateTime(1992, 08, 06).ToShortDateString();
+            var sobre = new SobreMimModel()
+            {
+                Nome = "Murillo Peteffi",
+                DataNascimento = new DateTime(1992, 08, 06),
+                AlturaCm = 170,
+                Peso = 65,
+                TipoSanguineo = "A+",
+                Gosta = "IPA, APA, Saison, Bitter, Belgian Blond Ale, Weizen, Dubbel, Tripel",
+                Desgosta = "Stout, Dunkel, Bock, Rauchbier",
+                Origem = "Brasil",
+                SlackNickname = "mpeteffi",
+                FacebookPage = "facebook.com/mpeteffi",
+                Email = "murillo.peteffi@cwi.com.br",
+                ImgUrl = "https://cloud.githubusercontent.com/assets/18291019/15033434/13d1ab3e-1241-11e6-9050-28b25e8e8d3f.png"
+            };
+            return View(sobre);
         }
     }
 }
