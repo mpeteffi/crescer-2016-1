ac8aa33a4370f26873da567db8a53989e30c1638
Murillo Peteffi <Murillo Peteffi>
2016-05-10 23:19:36 -0300

    feat(MVC-StreetFighter): criar SobreMimModel.

---
+++ src/modulo-05-dotnet/Aula 3/Exemplos/01 - MVC/MVC/MVC/Models/SobreMimModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class SobreMimModel
+    {
+        public String Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public int AlturaCm { get; set; }        
+        public int Peso { get; set; }
+        public String TipoSanguineo { get; set; }
+        public String Gosta { get; set; }
+        public String Desgosta { get; set; }    
+        public String Origem { get; set; }
+        public String SlackNickname { get; set; }
+        public String FacebookPage { get; set; }
+        public String Email { get; set; }
+        public String ImgUrl { get; set; }
+    }
+}
