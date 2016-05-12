0e54e81a4ad6af9d771c3bf82901029f1c9d5035
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-05-12 17:11:38 -0300

    feat(CameloNinja): adicionar ao dominio classe Pedido e Tipo Pagamento.

---
+++ src/modulo-05-dotnet/CameloNinja/CameloNinja.Dominio/Pedido.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CameloNinja.Dominio
+{
+    public class Pedido
+    {
+        /// <summary>
+        /// Construtor utilizado para montar pedidos novos
+        /// </summary>
+        public Pedido(DateTime dataEntregaDesejada, string nomeProduto, decimal valor, TipoPagamento tipoPagamento, string nomeCliente, string cidade, string estado)
+        {
+            DataEntregaDesejada = dataEntregaDesejada;
+            NomeProduto = nomeProduto;
+            Valor = valor;
+            TipoPagamento = tipoPagamento;
+            NomeCliente = nomeCliente;
+            Cidade = cidade;
+            Estado = estado;
+
+            //DateTime.Now contaria as horas, minutos e segundos, isso inviabliziaria algumas validações a seguir
+            DataPedido = DateTime.Today;
+
+            var diasRestantesParaConcluirEntrega = (dataEntregaDesejada - DataPedido).TotalDays;
+            ValidaPossibilidadeEntrega(diasRestantesParaConcluirEntrega);
+            DefineUrgenciaDoPedido(diasRestantesParaConcluirEntrega);
+        }
+
+        /// <summary>
+        /// Construtor utilizado para montar pedidos recuperados do repositório
+        /// </summary>
+        public Pedido(int id, DateTime dataPedido, DateTime dataEntregaDesejada, string nomeProduto, decimal valor, TipoPagamento tipoPagamento, string nomeCliente, string cidade, string estado, bool pedidoUrgente)
+        {
+            Id = id;
+            DataEntregaDesejada = dataEntregaDesejada;
+            NomeProduto = nomeProduto;
+            Valor = valor;
+            TipoPagamento = tipoPagamento;
+            NomeCliente = nomeCliente;
+            Cidade = cidade;
+            Estado = estado;
+            DataPedido = dataPedido;
+            PedidoUrgente = pedidoUrgente;
+        }
+
+        public int Id { get; private set; }
+        public DateTime DataPedido { get; private set; }
+        public DateTime DataEntregaDesejada { get; private set; }
+        public string NomeProduto { get; private set; }
+        public decimal Valor { get; private set; }
+        public TipoPagamento TipoPagamento { get; private set; }
+        public string NomeCliente { get; private set; }
+        public string Cidade { get; private set; }
+        public string Estado { get; private set; }
+        public bool PedidoUrgente { get; private set; }
+
+        private void ValidaPossibilidadeEntrega(double diasRestantesParaConcluirEntrega)
+        {
+            if (diasRestantesParaConcluirEntrega < 1)
+                throw new ArgumentException("A data de entrega desejada deve ser no mínimo 1 dia maior do que a data atual.");
+        }
+
+        private void DefineUrgenciaDoPedido(double diasRestantesParaConcluirEntrega)
+        {
+            PedidoUrgente = diasRestantesParaConcluirEntrega < 7;
+        }
+    }
+}
--- src/modulo-05-dotnet/CameloNinja/CameloNinja.Dominio/TipoPagamento.cs
+++ src/modulo-05-dotnet/CameloNinja/CameloNinja.Dominio/TipoPagamento.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace CameloNinja.MVC.Models
+namespace CameloNinja.Dominio
 {
     public enum TipoPagamento
     {
