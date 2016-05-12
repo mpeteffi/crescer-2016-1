c285645e1732211ac008a9221b1292fabc48546c
Murillo Peteffi <Murillo Peteffi>
2016-05-12 20:47:57 -0300

    feat(CameloNinja): adicionar referencias.

--- src/modulo-05-dotnet/CameloNinja/CameloNinja.Dominio/Pedido.cs
+++ src/modulo-05-dotnet/CameloNinja/CameloNinja.Dominio/Pedido.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
