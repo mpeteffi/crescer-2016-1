937ae7ac136a2578a6fd9cca4b4fde0ede78d25e
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-05-12 17:10:56 -0300

    feat(CameloNinja): adicionar classe RepositorioVendas.

---
+++ src/modulo-05-dotnet/CameloNinja/CameloNinja.Repositorio/RepositorioVendas.cs
@@ -0,0 +1,70 @@
+﻿using CameloNinja.Dominio;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CameloNinja.Repositorio
+{
+    public class RepositorioVendas
+    {
+        private const string PATH_ARQUIVO = "vendas.txt";
+
+        public List<Pedido> ObterPedidos()
+        {
+            var linhasArquivo = File.ReadAllLines(PATH_ARQUIVO).ToList();
+
+            return ConverteLinhasEmPedidos(linhasArquivo);
+        }
+
+        public Pedido ObterPedidoPorId(int id)
+        {
+            return this.ObterPedidos().FirstOrDefault(x => x.Id == id);
+        }
+
+        public void IncluirPedido(Pedido pedido)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AtualizarPedido(Pedido pedido)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExcluirPedido(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<Pedido> ConverteLinhasEmPedidos(List<string> linhasArquivo)
+        {
+            var listaPedidos = new List<Pedido>();
+
+            //Remove linha do cabeçalho
+            linhasArquivo.RemoveAt(0);
+
+            foreach (var linha in linhasArquivo)
+            {
+                var id = Convert.ToInt32(linha.Split(';')[0]);
+                var dataPedido = Convert.ToDateTime(linha.Split(';')[1]);
+                var dataEntregaDesejada = Convert.ToDateTime(linha.Split(';')[2]);
+                var nomeProduto = linha.Split(';')[3];
+                var valorVenda = Convert.ToDecimal(linha.Split(';')[4]);
+                TipoPagamento tipoPagamento;
+                Enum.TryParse(linha.Split(';')[5], out tipoPagamento);
+                var nomeCliente = linha.Split(';')[6];
+                var cidade = linha.Split(';')[7];
+                var estado = linha.Split(';')[8];
+                var urgente = Convert.ToBoolean(linha.Split(';')[9]);
+
+                var pedido = new Pedido(id, dataPedido, dataEntregaDesejada, nomeProduto, valorVenda, tipoPagamento, nomeCliente, cidade, estado, urgente);
+                listaPedidos.Add(pedido);
+            }
+
+            return listaPedidos;
+        }
+    }
+}
