d7f8c4e0e0cc1915371122a87f5dc70a2e6627a5
Murillo Peteffi <Murillo Peteffi>
2016-05-19 23:27:51 -0300

    feat(AJAX): Adicionar comportamento adicional à busca no caso de buscas por Justin Bieber.

--- src/modulo-06-ajax/dia01/Tema/script2.js
+++ src/modulo-06-ajax/dia01/Tema/script2.js
@@ -3,15 +3,19 @@
 $(function(){
   $('#buscar').submit(function(event){
     var nomeDaBanda = $('input[name=inputBusca]').val();
+    var douchebag = (nomeDaBanda == 'Justin Bieber' && Math.floor(Math.random() * 10) > 1); //retorna numeros de 0 a 9;
     var artistAPI = "https://api.spotify.com/v1/search?q=" + nomeDaBanda + "&type=artist";
     $.ajax({ url:artistAPI, type: 'GET' })
       .done(function (res){
-        var idDaBanda = res.artists.items[0].id;
+        var idDaBanda = douchebag ? "douchebag" : res.artists.items[0].id;
         var albumAPI = "https://api.spotify.com/v1/artists/" + idDaBanda + "/albums?limit=50";
         $.ajax({ url: albumAPI, type: 'GET' })
           .done(function (band){
             var albuns = band.items;
             adicionarCapasNaTela(albuns);
+          })
+          .fail(function(band){
+            alert("Caro usuário, devido a um erro 400, não foi possível pesquisar por Justin Bieber");
           });
       });
     event.preventDefault();
