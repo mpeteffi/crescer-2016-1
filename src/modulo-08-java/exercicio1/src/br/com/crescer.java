75f4059274f26268d3ac11cd5ec4bf31cdb8fcfb
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-06-07 15:11:22 -0300

    refactor(Java): adicionar close ao scanner.

--- src/modulo-08-java/exercicio1/src/br/com/crescer.java
+++ src/modulo-08-java/exercicio1/src/br/com/crescer.java
@@ -10,13 +10,16 @@ public class crescer {
 
     public static void main(String[] args) {
         
-        try (final Scanner scanner = new Scanner(System.in)) {
+        final Scanner scanner = new Scanner(System.in);
+        try {
             System.out.print("Digite um número: ");
             int numero = scanner.nextInt();
             String parimpar = numero%2 == 0 ? "par" : "impar";
             System.out.println("O numero " + numero + " é " + parimpar + ".");
         } catch (Exception e) {
             // 
+        } finally {
+            scanner.close();            
         }
         
     }
