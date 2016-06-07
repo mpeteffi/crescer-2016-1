1355f4dd1950499d20569445f865b6f8aad71f96
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-06-07 17:18:49 -0300

    feat(Java-Aula01): adicionar exercicio 7.3

---
+++ src/modulo-08-java/aula1/src/main/java/br/com/crescer/aula1/exercicio73.java
@@ -0,0 +1,32 @@
+package br.com.crescer.aula1;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author murillo.peteffi
+ */
+
+public class exercicio73 {
+    
+    public static void main (String[] args) {
+        
+        final List<Estados> lista = Arrays.asList(Estados.values());
+        lista.sort();        
+        
+        StringBuffer listaOrdenada = new StringBuffer("");
+        
+        String separador = "";
+        for(Estados estado : lista){
+            listaOrdenada.append(separador).append(estado.getNome());
+            separador = ", ";
+        }
+        
+        System.out.println(listaOrdenada);
+        
+    }
+    
+}
+
