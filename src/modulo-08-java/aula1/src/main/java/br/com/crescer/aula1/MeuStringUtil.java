5af78979ae201f6f2065a34fc9329fd9decffd83
Murillo Peteffi <Murillo Peteffi>
2016-06-07 22:18:42 -0300

    feat(MeuStringUtil): adicionar classe com funções para strings.

---
+++ src/modulo-08-java/aula1/src/main/java/br/com/crescer/aula1/MeuStringUtil.java
@@ -0,0 +1,46 @@
+package br.com.crescer.aula1;
+import java.text.Collator;
+import java.util.Locale;
+
+/**
+ * @author Murillo
+ */
+public class MeuStringUtil {
+    
+    public static void main(String[] args){
+        System.out.println("Funções disponíveis: \n ehVazia()\n quantidadeVogais()\n inverter()\n ehPalindromo()");
+    }
+    
+    public boolean ehVazia(String aString){
+        
+        return aString.isEmpty() || aString.trim().length() == 0;
+    } 
+   
+    public int quantidadeVogais(String aString){
+        
+        int quantidadeVogais = 0;
+        String vogais = "aeiou";
+        for(int i = 0; i < aString.length(); i++){
+            if(vogais.indexOf(aString.charAt(i)) >= 0){
+                quantidadeVogais++;
+            }
+        }
+        
+        return quantidadeVogais;
+    }
+
+    public String inverter(String aString){
+        
+        return new StringBuilder(aString).reverse().toString();
+    }
+    
+    public boolean ehPalindromo(String aString){
+    
+        /* stackoverflow.com/questions/28833797/compare-strings-ignoring-accented-characters */
+        /* guj.com.br/t/comparar-strings-com-e-sem-acento/35545/21 */
+        final Collator collator = Collator.getInstance(new Locale("pt", "BR"));
+        collator.setStrength(Collator.NO_DECOMPOSITION);
+
+        return collator.compare(aString.trim().toLowerCase(), inverter(aString).trim().toLowerCase()) == 0;
+    }
+}
