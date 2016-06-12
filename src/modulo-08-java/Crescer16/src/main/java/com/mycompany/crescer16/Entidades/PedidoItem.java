769369dc7e04e4a9a1de5c38c14fa611629e05a8
Murillo Peteffi <Murillo Peteffi>
2016-06-12 15:26:55 -0300

    feat(PedidoItem): adicionar mapeamento da entidade PedidoItem.

---
+++ src/modulo-08-java/Crescer16/src/main/java/com/mycompany/crescer16/Entidades/PedidoItem.java
@@ -0,0 +1,97 @@
+package com.mycompany.crescer16.Entidades;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.SEQUENCE;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ * @author Murillo
+ */
+@Entity
+@Table(name = "PEDIDOITEM")
+public class PedidoItem implements Serializable {
+    @Id 
+    @GeneratedValue(strategy = SEQUENCE, generator = "SQPEDIDOITEM")
+    @SequenceGenerator(name = "SQPEDIDOITEM", sequenceName = "SQPEDIDOITEM", allocationSize = 1)
+    @Basic(optional = false)
+    @Column(name = "IDPEDIDOITEM")
+    private long id;
+    
+    @Basic(optional = false)
+    @Column(name = "QUANTIDADE")
+    private double quantidade;
+    
+    @Basic(optional = false)
+    @Column(name = "PRECOUNITARIO")
+    private double precoUnitario;
+    
+    @Basic(optional = false)
+    @Column(name = "SITUACAO")
+    private char situacao;
+
+    @ManyToOne
+    @JoinColumn(name = "IDPRODUTO")
+    private Produto produto;
+    
+    public PedidoItem(){}
+    
+    public PedidoItem(double quantidade, double precoUnitario, char situacao, Produto produto) {
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+        this.situacao = situacao;
+        this.produto = produto;
+    }
+    
+    public PedidoItem(long id, double quantidade, double precoUnitario, char situacao, Produto produto) {
+        this(quantidade, precoUnitario, situacao, produto);
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public char getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(char situacao) {
+        this.situacao = situacao;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+}
