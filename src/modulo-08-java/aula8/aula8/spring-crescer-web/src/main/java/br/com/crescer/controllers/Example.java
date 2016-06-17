79d573387dabe75cf966abcd81c4b46edbef9e0e
Murillo Henrique de Mello Peteffi <murillo.peteffi@cwinet.local>
2016-06-17 16:22:14 -0300

    feat(aula8): adicionar actions com retorno em JSON.

--- src/modulo-08-java/aula8/aula8/spring-crescer-web/src/main/java/br/com/crescer/controllers/Example.java
+++ src/modulo-08-java/aula8/aula8/spring-crescer-web/src/main/java/br/com/crescer/controllers/Example.java
@@ -1,9 +1,16 @@
 package br.com.crescer.controllers;
 
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author murillo.peteffi
@@ -15,5 +22,18 @@ public class Example {
     String toIndex(@RequestParam(required = false) String name, Model model) {
         model.addAttribute("name", name != null ? name : "Murillo");
         return "index";
+    }
+    
+    @ResponseBody
+    @RequestMapping(value = "/current_date_time")
+    public Date date() {
+        return new Date();
+    }
+    
+    @ResponseBody
+    @RequestMapping(value = "/pessoa.json", method = RequestMethod.POST)
+    public List<Pessoa> list(@RequestBody Pessoa p) {
+        p.setNascimento(new Date());
+        return Stream.of(p).collect(Collectors.toList());
     }
 }
