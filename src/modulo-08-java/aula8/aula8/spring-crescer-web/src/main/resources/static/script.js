fea96aff6de645d60e9261de385c4e19ed434da7
Murillo Peteffi <Murillo Peteffi>
2016-06-18 00:28:36 -0300

    fix(Script): corrigir método de correção.

--- src/modulo-08-java/aula8/aula8/spring-crescer-web/src/main/resources/static/script.js
+++ src/modulo-08-java/aula8/aula8/spring-crescer-web/src/main/resources/static/script.js
@@ -13,6 +13,6 @@ function removerEntidade(pessoaId){
     $.ajax({url:'rest/pessoa/delete', type: 'POST', data: { id : pessoaId}})
         .done(function(){
             var tagBusca = 'button[data-pessoa-id=' + pessoaId + ']';
-            $(tagBusca).parents('tr').hide();
+            $(tagBusca).parents('tr').remove();
         });
 }
