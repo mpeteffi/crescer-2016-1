e5b908836f56bf2b92913cb6d91615aceb8e5ab3
Murillo Peteffi <Murillo Peteffi>
2016-06-17 23:07:51 -0300

    feat(PessoaService): adicionar serviços à classe pessoa.

--- src/modulo-08-java/aula8/aula8/spring-crescer-web/src/main/java/br/com/crescer/services/PessoaService.java
+++ src/modulo-08-java/aula8/aula8/spring-crescer-web/src/main/java/br/com/crescer/services/PessoaService.java
@@ -1,10 +1,8 @@
 package br.com.crescer.services;
 
-import br.com.crescer.controllers.Pessoa;
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import br.com.crescer.entity.Pessoa;
+import br.com.crescer.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,10 +11,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class PessoaService {
 
-    public List<Pessoa> list() {
-        Pessoa p = new Pessoa();
-        p.setNome("Murillo Peteffi");
-        p.setNascimento(new Date());
-        return Stream.of(p).collect(Collectors.toList());
+    @Autowired
+    PessoaRepository repository;
+    
+    public Iterable<Pessoa> findAll() {
+        return repository.findAll();
+    }
+
+    public Pessoa save(Pessoa p) {
+        return repository.save(p);
+    }
+
+    public void delete(Long id) {
+        repository.delete(id);
     }
 }
