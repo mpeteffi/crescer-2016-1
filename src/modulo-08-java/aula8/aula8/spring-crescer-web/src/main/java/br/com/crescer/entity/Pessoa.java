58953238a0e70ad3ea8547736f19e0e960987e29
Murillo Peteffi <Murillo Peteffi>
2016-06-17 23:07:25 -0300

    refactor(Pessoa): mapear classe pessoa com anotações para seguir padrão do banco de dados.

--- src/modulo-08-java/aula8/aula8/spring-crescer-web/src/main/java/br/com/crescer/entity/Pessoa.java
+++ src/modulo-08-java/aula8/aula8/spring-crescer-web/src/main/java/br/com/crescer/entity/Pessoa.java
@@ -1,34 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package br.com.crescer.controllers;
+package br.com.crescer.entity;
 
+import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
- *
  * @author murillo.peteffi
  */
-public class Pessoa {
-    
-    private Date nascimento;
+@Entity
+@Table(name = "PESSOA")
+public class Pessoa implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_PESSOA")
+    @SequenceGenerator(name = "SEQ_PESSOA", sequenceName = "SEQ_PESSOA", allocationSize = 1)
+    @Basic(optional = false)
+    @Column(name = "ID_PESSOA")
+    private Long id;
+    @Basic(optional = false)
+    @Column(name = "NM_PESSOA")
     private String nome;
+    @Temporal(value = TemporalType.DATE)
+    @Basic(optional = false)
+    @Column(name = "DT_NASCIMENTO_PESSOA")
+    private Date nascimento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getNome() {
         return nome;
     }
-    
-    public void setNascimento(Date Nascimento) {
-        this.nascimento = Nascimento;
-    }
-    
+
     public void setNome(String nome) {
         this.nome = nome;
     }
 
     public Date getNascimento() {
         return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
     }
 }
